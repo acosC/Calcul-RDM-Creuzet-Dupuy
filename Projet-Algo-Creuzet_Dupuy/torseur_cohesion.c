253fd0ac536e0821f9dcfb32975fb70886e32b3a
MargueritePt <50578607+MargueritePt@users.noreply.github.com>
2019-05-15 16:39:59 +0200

    Add files via upload

---
+++ Projet-Algo-Creuzet_Dupuy/torseur_cohesion.c
@@ -0,0 +1,35 @@
+#include "rdm.h"
+#include "gnuplot_i.h"
+
+torseur_cohesion* Initialisation_cohesion(torseur_cohesion tc){
+
+  int i;
+
+  for (i = 0 ; i < 3 ; i++){
+    tc.f[i] = 0;
+    tc.m[i] = 0;
+  }
+  printf("%lf\n",tc.f[0]);
+  return &tc;
+}
+
+void Affichercourbe(torseur *t, int *ptr_longueur){
+
+  int i, limite = 20;
+  double x[157], y[157];
+  gnuplot_ctrl *h;      //on touche pas à ça
+  h = gnuplot_init();   //pas touche
+
+  gnuplot_setstyle(h, "lines");   //pas touche deco
+
+  for (i = 0 ; i <= *ptr_longueur ; i++){
+
+    x[i] = (double)i;
+    y[i] = (double)i + 5;
+
+  }
+  gnuplot_plot_xy(h, x, y, *ptr_longueur, "TEST :)");
+  sleep(2);
+
+  return;
+}
